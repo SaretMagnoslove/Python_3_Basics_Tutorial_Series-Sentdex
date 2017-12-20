b6ef1a0206aec0185d0e7896cbd1e456e8773646
SaretMagnoslove <magnoslove@gmail.com>
2017-12-20 20:44:54 +0200

    Add up to Lesson37 and a build libray with exe

---
+++ setup.py
@@ -0,0 +1,6 @@
+from cx_Freeze import setup, Executable
+
+setup(name = "reandurllib" ,
+      version = "0.1" ,
+      description = "" ,
+      executables = [Executable("Lesson37_cx_freeze.py")])
